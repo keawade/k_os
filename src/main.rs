726267961c9a565b3ed9cbc1e381aad887b18aaa
Keith Wade <keawade@protonmail.com>
2020-10-17 18:55:31 -0500

    VGA buffer, print/println macros

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,25 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"k_os minimal kernel successfully boot loaded!";
+// static HELLO: &[u8] = b"k_os minimal kernel successfully boot loaded!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    // this function is the entry point, since the linker looks for a function named `_start` by default
 
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("k_os minimal kernel successfully boot loaded!");
+    println!();
+    println!("print and println macros implemented!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    panic!("Panic handler now reports")
+    // loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
