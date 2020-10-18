8c349199c31dd4a32cff5a05ce2e756b8343dbf5
Keith Wade <keawade@protonmail.com>
2020-10-17 19:44:27 -0500

    Implement test runner connected to QEMU

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+mod serial;
 mod vga_buffer;
 
 // static HELLO: &[u8] = b"k_os minimal kernel successfully boot loaded!";
@@ -11,16 +15,71 @@ pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function named `_start` by default
 
     println!("k_os minimal kernel successfully boot loaded!");
-    println!();
-    println!("print and println macros implemented!");
 
-    panic!("Panic handler now reports")
-    // loop {}
+    #[cfg(test)]
+    test_main();
+
+    loop {}
 }
 
-/// This function is called on panic.
+// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("\nRunning {} tests\n", tests.len());
+    for test in tests {
+        test.run(); // new
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(0, 0);
 }
